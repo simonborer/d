9ba5cd15e70662e25c834405f11cbd4e8699674a
simon.borer <simon.borer@gmail.com>
2022-05-19 13:43:06 -0400

    alt layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,24 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const readingTime = require("eleventy-plugin-reading-time");
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600, 1200],
+    formats: ["avif", "webp", "jpeg"]
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(pluginRss);
@@ -20,6 +38,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addShortcode("br", function() {
         return `<br aria-hidden="true">`;
     });
+
+    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
     eleventyConfig.addPairedNunjucksShortcode("ext", function(content, href) {return `<a href="${href}" target="_blank" rel="noreferrer">${content}</a>`});
 
--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ const animDefaults = {
 };
 
 annElem.forEach((e) => {
-  // if (e.dataset.annotateImmediate === "true") {
     annotate(e, {
       type: e.dataset.annType ? e.dataset.annType : animDefaults.type,
       animate: e.dataset.annAnim ? e.dataset.annAnim : false,
@@ -21,27 +20,9 @@ annElem.forEach((e) => {
       strokeWidth: animDefaults.strokeWidth,
       padding: e.dataset.annPadding ? e.dataset.annPadding : animDefaults.padding
     }).show();
-  // } else {
-  //     console.log(e);
-  //   const ant = annotate(e, {
-  //     type: e.dataset.annType ? e.dataset.annType : animDefaults.type,
-  //     animate: e.dataset.annAnim ? e.dataset.annAnim : true,
-  //     animationDuration: 2000,
-  //     color: e.dataset.annColor ? e.dataset.annColor : animDefaults.color,
-  //     multiline: true,
-  //     strokeWidth: animDefaults.strokeWidth,
-  //     padding: e.dataset.annPadding ? e.dataset.annPadding : animDefaults.padding
-  //   });
-  //   let observer = new IntersectionObserver(
-  //     (entries, self) => {
-  //       entries.forEach((entry) => {
-  //         if (entry.isIntersecting) {
-  //             console.log("at least this");
-  //           ant.show();
-  //         }
-  //       });
-  //     }, { threshold: 1.0 }
-  //   );
-  //   observer.observe(e);
-  // }
 });
+
+// Scroll variable
+window.addEventListener('scroll', () => {
+  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
