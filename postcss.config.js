7e43b385ff8c83ec249377b33809e8fcd9e1ce0d
simon.borer <simon.borer@gmail.com>
2022-05-20 09:20:33 -0400

    added purgecss and autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        purgecss({
+            content: ['_site/**/*.html'],
+            safelist: ['no-scroll', 'nav__links--expanded', 'nav__toggle-tap-to-close--open']
+        })
+    ]
+}
